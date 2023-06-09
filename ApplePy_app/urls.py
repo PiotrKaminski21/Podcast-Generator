8b9e712180ad806fe6b56e727823221c8209cbe1
Aleksandra Jędrzejczyk <118756898+OlaJedrzejczyk@users.noreply.github.com>
2023-06-09 18:43:07 +0200

    Add files via upload
    projekt 1.3
    superuser: 
    login: apple
    hasło: zdpytpol62

--- ApplePy_app/urls.py
+++ ApplePy_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import HomeView, GenreFormView, ResultView
+from . import views
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('form/', GenreFormView.as_view(), name='form'),
     path('result/', ResultView.as_view(), name='result'),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('signout', views.signout, name="signout"),
 ]
--- ApplePy_app/views.py
+++ ApplePy_app/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -133,7 +136,73 @@ class GenreFormView(View):
             message = "Invalid genre name."
             return render(request, 'result.html', {'message': message, 'genres': all_genres})
 
+
 #tutaj mamy klasę wyników - to wyplute dane + render po templatce
 class ResultView(View):
     def get(self, request):
-        return redirect('home')
+        return render(request, 'home')
+
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists! Please try other username")
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered")
+            return redirect('home')
+
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters")
+
+        if pass1 != pass2:
+            messages.error(request, "Passwords don't match")
+
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-numeric")
+            return redirect('home')
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created")
+
+        return redirect('signin')
+
+    return render(request, "signup.html")
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            # fname = user.first_name
+            return render(request, "form.html", {'genres': all_genres})
+        else:
+            messages.error(request, "Wrong details")
+            return redirect('form')
+
+    return render(request, "signin.html")
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged out successfully")
+    return redirect('home')
